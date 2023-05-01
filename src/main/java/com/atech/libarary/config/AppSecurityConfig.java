343fe308ec88a265e00d9f8bcf6146407f5506db
raedgeorge <raedgeorge2014@gmail.com>
2023-05-01 14:15:31 +0300

    push to main branch

---
+++ src/main/java/com/atech/libarary/config/AppSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.atech.libarary.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author raed abu Sa'da
+ * on 01/05/2023
+ */
+
+@Configuration
+public class AppSecurityConfig {
+}
